27600d70ee47d966e4b7dae74892ef1e5ff0c165
Preben Ingvaldsen <quakeaddict@gmail.com>
2014-02-21 20:21:49 -0800

    Add ability to rotate ship

---
+++ Assets/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		float xAxis = Input.GetAxis ("Horizontal");
+		float yAxis = Input.GetAxis ("Vertical");
+		transform.Rotate(new Vector3(yAxis, xAxis, 0.0f));
+	}
+}
