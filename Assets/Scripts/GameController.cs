fbb0faa8b462fca778ca4b18d6e7140de47073e3
Preben Ingvaldsen <quakeaddict@gmail.com>
2014-02-27 14:01:34 -0800

    Add asteroid as prefab

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject asteroid;
+
+	void Start() {
+		SpawnAsteroidWave ();
+	}
+
+	void SpawnAsteroidWave() {
+		Vector3 astPosition = new Vector3 ();
+		Quaternion astRotation = new Quaternion ();
+		Instantiate (asteroid, astPosition, astRotation);
+	}
+}
