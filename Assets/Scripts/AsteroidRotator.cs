298c53575a7b7338cd05bb7acfd068d91f90fb43
Preben Ingvaldsen <quakeaddict@gmail.com>
2014-02-27 12:31:04 -0800

    Add single asteroid to playfield with rotation

---
+++ Assets/Scripts/AsteroidRotator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidRotator : MonoBehaviour {
+	public float tumble;
+	void Start() {
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
