1a90f53dfefb2c247454be0f5f7a9c997f80ab3e
woundeddophin <justfrznh2o@gmail.com>
2014-03-02 15:30:30 -0800

    Adds lasers and firing

---
+++ Assets/Fire.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+	public GameObject shot;
+	public float fireRate;
+	public Transform shotSpawn;
+	private float nextFire;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+}
---
+++ Assets/reticle2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reticle2 : MonoBehaviour {
+	public Texture2D crosshairImage;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnGUI()
+	{
+		float xMin = (Screen.width / 2) - (crosshairImage.width / 2);
+		float yMin = (Screen.height / 2) - (crosshairImage.height / 2);
+		GUI.DrawTexture(new Rect(xMin+crosshairImage.width/4, yMin+crosshairImage.height/4, crosshairImage.width/2, crosshairImage.height/2), crosshairImage);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
